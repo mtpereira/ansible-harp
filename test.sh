692fa97d57356d187d604f6883c46994c7b2a600
Manuel Tiago Pereira <mt.pereira@gmail.com>
2014-05-26 15:17:59 +0100

    No longer using my forked Ansible.nodejs, changes were accepted.

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ansible-galaxy install --ignore-errors Ansibles.nodejs Ansibles.build-essential
+ansible-galaxy install --ignore-errors --force Ansibles.nodejs Ansibles.build-essential
 vagrant up
